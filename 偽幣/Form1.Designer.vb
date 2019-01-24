ed8badb23e8b153b64055226d1b5bbe4f72cf9f2
ccmikechen <ccmikechen@gmail.com>
2019-01-24 10:22:12 +0800

    Initial commit

---
+++ 偽幣/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "START"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 36)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(52, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(86, 12)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(49, 17)
+        Me.RadioButton1.TabIndex = 2
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "較輕"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(86, 36)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(49, 17)
+        Me.RadioButton2.TabIndex = 3
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "較重"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "偽幣位置"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 73)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(200, 206)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 291)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ 偽幣/Form1.vb
@@ -0,0 +1,127 @@
+﻿
+Public Class Form1
+    Dim HL As String
+    Dim P As String
+    Sub check(ByVal m() As Integer)
+
+        Dim s1(3), s2(3), r1(3), r2(3), n1, n2 As Integer
+        For i = 0 To 3
+            n1 += Val(m(i))
+            n2 += Val(m(i + 4))
+        Next
+
+        TextBox2.Text &= "第一次" & vbCrLf & "左 1 2 3 4" & vbTab & "右 5 6 7 8" & vbCrLf
+        If n1 = n2 Then
+            TextBox2.Text &= "左邊重量等於右邊重量" & vbCrLf & vbCrLf
+            C1(Val(m(8)), Val(m(9)), Val(m(10)), Val(m(11)), m(0)) : Exit Sub
+        ElseIf n1 > n2 Then
+            TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+            For i = 0 To 3
+                s1(i) = m(i) : s2(i) = m(i + 4)
+                r1(i) = i + 1 : r2(i) = i + 5
+            Next
+        Else
+            TextBox2.Text &= "左邊重量小於右邊重量" & vbCrLf & vbCrLf
+            For i = 0 To 3
+                s1(i) = m(i + 4) : s2(i) = m(i)
+                r1(i) = i + 5 : r2(i) = i + 1
+            Next
+        End If
+
+        Dim t1(2), t2(2), t3(1), ts1, ts2 As Integer
+        t1(0) = s1(0) : t1(1) = s1(1) : t1(2) = s2(0)
+        t2(0) = s1(2) : t2(1) = s1(3) : t2(2) = s2(1)
+        t3(0) = s2(2) : t3(1) = s2(3)
+        For i = 0 To 2
+            ts1 += t1(i) : ts2 += t2(i)
+        Next
+
+        TextBox2.Text &= "第二次" & vbCrLf & "左 " & r1(0) & " " & r1(1) & " " & _
+            r2(0) & vbTab & "右 " & r1(2) & " " & r1(3) & " " & r2(1) & vbCrLf
+        If ts1 = ts2 Then
+            TextBox2.Text &= "左邊重量等於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "第三次" & vbCrLf & "左 " & r2(2) & vbTab & "右 " & r2(3) & vbCrLf
+            If t3(0) > t3(1) Then
+                TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+                TextBox2.Text &= "結果：偽幣較輕，第" & r2(3) & "個"
+            Else
+                TextBox2.Text &= "左邊重量小於右邊重量" & vbCrLf & vbCrLf
+                TextBox2.Text &= "結果：偽幣較輕，第" & r2(2) & "個"
+            End If
+        ElseIf ts1 > ts2 Then
+            TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "第三次" & vbCrLf & "左 " & r1(0) & vbTab & "右 " & r1(1) & vbCrLf
+            C2(t1(0), t1(1), {r1(0), r1(1), r2(1)}) : Exit Sub
+        Else
+            TextBox2.Text &= "左邊重量小於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "第三次" & vbCrLf & "左 " & r1(2) & vbTab & "右 " & r1(3) & vbCrLf
+            C2(t2(0), t2(1), {r1(2), r1(3), r2(0)}) : Exit Sub
+        End If
+
+    End Sub
+
+    Sub C1(ByVal n1, ByVal n2, ByVal n3, ByVal n4, ByVal rw)
+        TextBox2.Text &= "第二次" & vbCrLf & "左 1 2 3" & vbTab & "右 9 10 11" & vbCrLf
+        If n1 + n2 + n3 = rw * 3 Then
+            TextBox2.Text &= "左邊重量等於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "第三次" & vbCrLf & "左 1" & vbTab & "右 12" & vbCrLf
+            If rw > n4 Then
+                TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+                TextBox2.Text &= "結果：偽幣較輕，第12個"
+            Else
+                TextBox2.Text &= "右邊重量大於左邊重量" & vbCrLf & vbCrLf
+                TextBox2.Text &= "結果：偽幣較重，第12個"
+            End If
+            Exit Sub
+        ElseIf n1 + n2 + n3 < rw * 3 Then
+            TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "第三次" & vbCrLf & "左 9" & vbTab & "右 10" & vbCrLf
+            HL = "輕"
+        Else
+            TextBox2.Text &= "左邊重量小於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "第三次" & vbCrLf & "左 9" & vbTab & "右 10" & vbCrLf
+            HL = "重"
+        End If
+        If n1 = n2 Then
+            TextBox2.Text &= "左邊重量等於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "結果：偽幣較" & HL & "，第11個"
+        ElseIf n1 > n2 Then
+            TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+            P = IIf(HL = "重", 9, 10)
+            TextBox2.Text &= "結果：偽幣較" & HL & "，第" & P & "個"
+        Else
+            TextBox2.Text &= "左邊重量小於右邊重量" & vbCrLf & vbCrLf
+            P = IIf(HL = "輕", 9, 10)
+            TextBox2.Text &= "結果：偽幣較" & HL & "，第" & P & "個"
+        End If
+
+    End Sub
+
+    Sub C2(ByVal n1, ByVal n2, ByVal r())
+        If n1 = n2 Then
+            TextBox2.Text &= "左邊重量等於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "結果：偽幣較輕" & "，第" & r(2) & "個"
+        ElseIf n1 > n2 Then
+            TextBox2.Text &= "左邊重量大於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "結果：偽幣較重" & "，第" & r(0) & "個"
+        Else
+            TextBox2.Text &= "左邊重量小於右邊重量" & vbCrLf & vbCrLf
+            TextBox2.Text &= "結果：偽幣較重" & "，第" & r(1) & "個"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox2.Clear()
+        Dim n As Integer = TextBox1.Text
+        If n < 1 Or n > 12 Then TextBox2.Text = "輸入範圍1~12" : Exit Sub
+        Dim w As Integer = IIf(RadioButton1.Checked, 0, 1)
+        Dim st(11) As Integer
+        For i = 0 To 11
+            If i = n - 1 Then st(i) = w Else st(i) = IIf(w = 0, 1, 0)
+        Next
+        check(st)
+
+    End Sub
+
+   
+End Class
